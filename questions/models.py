c3796162c1b5639ead624728d485b4e6432f23a7
Choseoyoung <seoyoung0000@naver.com>
2022-05-20 10:42:19 +0900

    Model 만들기

--- questions/models.py
+++ questions/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    title = models.CharField(max_length = 200)
+    subject = models.CharField(max_length = 50)
+    create_date = models.DateTimeField()
+    content = models.TextField()
+    ans1 = models.TextField()
+    ans2 = models.TextField()
+    ans3 = models.TextField()
+    ans4 = models.TextField()
+    ans = models.TextField()
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete = models.CASCADE)
+    content = models.TextField()
+    create_date = models.DateTimeField()
